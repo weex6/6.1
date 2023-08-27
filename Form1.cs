17dad7542b78511f3e2b816f15479970c9899533
79113 <79113@LAPTOP-5IF5OQEN>
2023-08-27 12:48:40 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMakeOrder_Click(object sender, EventArgs e)
+        {
+            if (domType.SelectedIndex < 0 || domSize.SelectedIndex < 0)
+                MessageBox.Show("Укажите тип вещи и размер");
+            else
+            {
+                Order order = new Order();
+                order.Launch(domType.SelectedItem.ToString(), domSize.SelectedItem.ToString());
+                DialogResult dialogResult = MessageBox.Show($"Ваш заказ {order.singleton.type} размера {order.singleton.size}. Верно?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Random rand = new Random();
+                    int r = rand.Next(1, 5);
+                    if (r == 1)
+                    {
+                        MessageBox.Show("Мы приступили к выполнению вашего заказа", "Ожидайте", MessageBoxButtons.OK);
+                        MessageBox.Show("Ваш заказ готов. Вы можете его забрать.", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    if (r == 2)
+                    {
+                        MessageBox.Show("Извините, мыши сгрызли все нитки...", "", MessageBoxButtons.OK);
+                    }
+                    if (r == 3)
+                    {
+                        MessageBox.Show("Случилось перенапряжение сети и швейные машины перестали работать", "Неполадочки", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    if (r == 4)
+                    {
+                        MessageBox.Show("Король запретил носить одежду и мы не можем выполнить ваш заказ", "Указ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1
+{
+    internal class Singleton
+    {
+        private static Singleton instance;
+        public string type { get; private set; }
+        public string size { get; private set; }
+        private Singleton(string _type, string _size)
+        {
+            type = _type;
+            size = _size;
+        }
+        public static Singleton GetInstance(string _type, string _size)
+        {
+            if (instance == null)
+                instance = new Singleton(_type, _size);
+            return instance;
+        }
+    }
+    class Order
+    {
+        public Singleton singleton { get; set; }
+        public void Launch(string type, string size)
+        {
+            singleton = Singleton.GetInstance(type, size);
+        }
+    }
+}
